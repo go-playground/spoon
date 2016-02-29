c3e68e43424ff0f75a237be0fba3e0ba5e5e08b6
joeybloggs <Dean.Karn@gmail.com>
2016-02-29 17:52:56 -0500

    Add example setup file to avoid huge binary sizes.

--- examples/basic/setup.sh
+++ examples/basic/setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 cd client
-sed -i -e 's/Old/New/g' main.go
+sed -i.bak -e 's/Old /New /g' main.go
 go build
 mv client ../server/files
-sed -i -e 's/New/Old/g' main.go
+sed -i.bak -e 's/New /Old /g' main.go
 go build
+rm main.go.bak
